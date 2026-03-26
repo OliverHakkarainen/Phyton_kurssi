6590da1e88f621583ff86db205281ecd00b7b2bc
oliver <oliver.hakkarainen@metropolia.fi>
2026-03-26 08:58:52 +0200

    tehty

--- Tehtavat/Viikko6/Tehtava6.py
+++ Tehtavat/Viikko6/Tehtava6.py
@@ -7,8 +7,6 @@ def pizzan_yksikkohinta(halkaisija, hinta):
     yksikkohinta = hinta / pintaala_m2
     return yksikkohinta
 
-# Kysytään pizzojen tiedot
-
 halkaisija1 = float(input("Anna ekan pizzan halkaisija: "))
 hinta1 = float(input("Anna ekan pizzan hinta: "))
 halkaisija2 = float(input("Anna tokan pizzan halkaisija: "))
@@ -17,7 +15,6 @@ hinta2 = float(input("Anna tokan pizzan hinta: "))
 pizza1 = pizzan_yksikkohinta(halkaisija1, hinta1)
 pizza2 = pizzan_yksikkohinta(halkaisija2, hinta2)
 
-# Verrataan
 
 if pizza1 < pizza2:
     print("Parempi vastine rahalle on eka pizza")
